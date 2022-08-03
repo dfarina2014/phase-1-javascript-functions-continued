d966d6e4ab3197d2b4e1afff0f0dafa0e173c303
daren farina <daren.farina@gmail.com>
2022-08-03 09:50:36 -0400

    commit tree 5a311d748ee97f6def8ec40d8fd10d602cd5909b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(target = 'roller-skate') {
+    return (`This Saturday, I want to ${target}!`);
+}
+saturdayFun();
+
+function mondayWork(target = 'go to the office') {
+    return (`This Monday, I will ${target}.`);
+}
+mondayWork();
+
+function wrapAdjective(otherTarget = '*') {
+    return function (target = 'special') {
+        return (`You are ${otherTarget}${target}${otherTarget}!`)
+        
+    }
+}
+
